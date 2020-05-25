c10e32fec73034bdbaf8df3b528485e59f8e93ed
verlopezsa <verlopezsa@gmail.com>
2020-05-25 18:58:27 +0200

    Cambiios 2

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -11,14 +11,23 @@
 // var byeSpeaker =
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-var speakWord = "Good Bye";
+(function(window) {
+	var byeSpeaker = {};
+	byeSpeaker.name = name;
+	var speakWord = "Good Bye";
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-function speak(name) {
-  console.log(speakWord + " " + name);
-}
+	byeSpeaker.speak = function () {
+  		console.log(speakWord + " " + name);
+  	}
+
+  	window.byeSpeaker = byeSpeaker;
+//	function speak(name) {
+// 		console.log(speakWord + " " + name);
+//  	}
+})(window);
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
